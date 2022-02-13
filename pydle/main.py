6f04ffbf96facfa739deb37ef6a8397413567577
thom pyle <99365003+th0mpyle@users.noreply.github.com>
2022-02-13 17:57:51 +0000

    update loss output

--- pydle/main.py
+++ pydle/main.py
@@ -68,6 +68,7 @@ if __name__ == "__main__":
                 tries += 1
             if tries == 6:
                 print("You lose!")
+                print(f"The answer was {target.upper()}.")
         
 
 
